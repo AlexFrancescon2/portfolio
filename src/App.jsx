adbdb4623c40028abb7ee4b0dce1f02aa581c18f
Alex Francescon <alex.francescon@hotmail.it>
2024-04-17 15:10:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <>My tokens</>;
+}
+
+export default App;
---
+++ src/styles/tokens.js
@@ -0,0 +1,75 @@
+export const colors = {};
+
+export const shadows = colors;
+
+export const fonts = {
+  normal:
+    "Roboto Custom, -apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif",
+  mono: "Menlo, Consolas, monospace",
+};
+
+export const space = {
+  1: "2px",
+  2: "4px",
+  3: "8px",
+  4: "16px",
+  5: "24px",
+  6: "32px",
+  7: "40px",
+  8: "48px",
+  9: "56px",
+  10: "64px",
+};
+
+export const sizes = {
+  1: "2px",
+  2: "4px",
+  3: "8px",
+  4: "16px",
+  5: "24px",
+  6: "32px",
+  header: "60px",
+};
+
+export const fontSizes = {
+  1: "12px",
+  2: "13px",
+  3: "14px",
+  4: "15px",
+  5: "17px",
+  6: "20px",
+  7: "21px",
+  8: "27px",
+  9: "35px",
+  10: "45px",
+  11: "55px",
+  12: "70px",
+  13: "85px",
+};
+
+export const zIndices = {
+  1: "100",
+  2: "200",
+  3: "300",
+  4: "400",
+  max: "999",
+};
+
+export const media = {
+  bp1min: "(min-width: 640px)",
+  bp1max: "(max-width: 640px)",
+  bp2min: "(min-width: 768px)",
+  bp2max: "(max-width: 768px)",
+  bp3min: "(min-width: 1024px)",
+  bp3max: "(max-width: 1024px)",
+  bp4min: "(min-width: 1400px)",
+  bp4max: "(max-width: 1400px)",
+  motion: "(prefers-reduced-motion)",
+  hover: "(any-hover: hover)",
+  dark: "(prefers-color-scheme: dark)",
+  light: "(prefers-color-scheme: light)",
+};
+
+export const boxShadows = {
+  1: "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+};
